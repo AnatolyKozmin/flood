efc0a685dea8c918526dd0cd5fbbca90ae0410c3
lerowegr <139485624+lerowegr@users.noreply.github.com>
2026-09-20 15:01:17 +0300

    удалить шмальнуть и мефчик полностью

--- main.py
+++ main.py
@@ -22,8 +22,6 @@ from handlers.command_candle import candle_router
 from handlers.command_seabattle import seabattle_router
 from handlers.command_kalik import kalik_router
 from handlers.command_bar import bar_router
-from handlers.command_shmal import shmal_router
-from handlers.command_mef import mef_router
 from handlers.command_mod import mod_router
 from handlers.command_admin import admin_router
 from middlewares.message_counter import MessageCounterMiddleware, flush_stats
@@ -61,8 +59,6 @@ dp.include_router(candle_router)
 dp.include_router(seabattle_router)
 dp.include_router(kalik_router)
 dp.include_router(bar_router)
-dp.include_router(shmal_router)
-dp.include_router(mef_router)
 dp.include_router(top_router)
 dp.include_router(all_router)
 
